b3296010a7287d418d739637c10ae77fd315274b
Vincent van Dijck <vincent@voorhoede.nl>
2018-10-22 15:55:36 +0200

    fix(setup) update for iOS

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -12,8 +12,8 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-bluetoothle": "4.4.4"
+  "cordova-plugin-bluetoothle": "4.4.4",
+  "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
