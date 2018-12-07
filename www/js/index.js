4f15fb6204e928f07753f2be48b08f915ff6edb4
Anand Graves <anand@voorhoede.nl>
2018-12-07 10:27:38 +0100

    small design changes, config changes

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var battery = {
-    service: "180F",
-    level: "2A19"
+	service: "180F",
+	level: "2A19"
 };
 
 var mainPage = document.getElementById('main-page');
@@ -14,79 +14,79 @@ var batteryStateButton = document.getElementById('battery-state-button');
 var disconnectButton = document.getElementById('disconnect-button');
 
 var app = {
-    initialize: function() {
-        this.bindEvents();
-        detailPage.hidden = true;
-    },
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        refreshButton.addEventListener('click', this.refreshDeviceList, false);
-        batteryStateButton.addEventListener('click', this.readBatteryState, false);
-        disconnectButton.addEventListener('click', this.disconnect, false);
-        deviceList.addEventListener('click', this.connect, false);
-    },
-    onDeviceReady: function() {
-        app.refreshDeviceList();
-    },
-    refreshDeviceList: function() {
-        deviceList.innerHTML = '';
-        ble.scan([], 5, app.onDiscoverDevice, app.onError);
-    },
-    onDiscoverDevice: function(device) {
-        if (typeof device.name === 'string') {
-            var listItem = document.createElement('li');
-            var html = `
+	initialize: function () {
+		this.bindEvents();
+		detailPage.hidden = true;
+	},
+	bindEvents: function () {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+		refreshButton.addEventListener('click', this.refreshDeviceList, false);
+		batteryStateButton.addEventListener('click', this.readBatteryState, false);
+		disconnectButton.addEventListener('click', this.disconnect, false);
+		deviceList.addEventListener('click', this.connect, false);
+	},
+	onDeviceReady: function () {
+		app.refreshDeviceList();
+	},
+	refreshDeviceList: function () {
+		deviceList.innerHTML = '';
+		ble.scan([], 5, app.onDiscoverDevice, app.onError);
+	},
+	onDiscoverDevice: function (device) {
+		if (typeof device.name === 'string') {
+			var listItem = document.createElement('li');
+			var html = `
                 <span>${device.name}</span>
                 <button class="btn btn-blue" data-device-id="${device.id}">Connect</button>
             `;
 
-            listItem.classList.add('list-item');
-            listItem.innerHTML = html;
-            deviceList.appendChild(listItem);
-        }
-    },
-    connect: function(e) {
-        if (e.target.classList.contains('btn-blue')) {
-            var deviceId = e.target.dataset.deviceId;
-            var onConnect = function() {
-                ble.startNotification(deviceId, battery.service, battery.level, app.onBatteryLevelChange, app.onError);
-                batteryStateButton.dataset.deviceId = deviceId;
-                disconnectButton.dataset.deviceId = deviceId;
-                app.showDetailPage();
-            };
-            ble.connect(deviceId, onConnect, app.onError);
-        }
-    },
-    onBatteryLevelChange: function(data) {
-        console.log('onBatteryLevelChange', data);
-        var batteryLevel = new Uint8Array(data);
-        batteryState.innerHTML = batteryLevel[0];
-    },
-    readBatteryState: function(event) {
-        console.log('readBatteryState');
-        var deviceId = event.target.dataset.deviceId;
-        ble.read(deviceId, battery.service, battery.level, app.onReadBatteryLevel, app.onError);
-    },
-    onReadBatteryLevel: function(data) {
-        console.log('onReadBatteryLevel', data);
-        var batteryLevel = new Uint8Array(data);
-        batteryState.innerHTML = batteryLevel[0];
-    },
-    disconnect: function(event) {
-        var deviceId = event.target.dataset.deviceId;
-        ble.disconnect(deviceId, app.showMainPage, app.onError);
-    },
-    showMainPage: function() {
-        mainPage.hidden = false;
-        detailPage.hidden = true;
-    },
-    showDetailPage: function() {
-        mainPage.hidden = true;
-        detailPage.hidden = false;
-    },
-    onError: function(reason) {
-        alert('ERROR: ' + JSON.stringify(reason, 0, 4)); // real apps should use notification.alert
-    }
+			listItem.classList.add('list-item');
+			listItem.innerHTML = html;
+			deviceList.appendChild(listItem);
+		}
+	},
+	connect: function (e) {
+		if (e.target.classList.contains('btn-blue')) {
+			var deviceId = e.target.dataset.deviceId;
+			var onConnect = function () {
+				ble.startNotification(deviceId, battery.service, battery.level, app.onBatteryLevelChange, app.onError);
+				batteryStateButton.dataset.deviceId = deviceId;
+				disconnectButton.dataset.deviceId = deviceId;
+				app.showDetailPage();
+			};
+			ble.connect(deviceId, onConnect, app.onError);
+		}
+	},
+	onBatteryLevelChange: function (data) {
+		console.log('onBatteryLevelChange', data);
+		var batteryLevel = new Uint8Array(data);
+		batteryState.innerHTML = batteryLevel[0];
+	},
+	readBatteryState: function (event) {
+		console.log('readBatteryState');
+		var deviceId = event.target.dataset.deviceId;
+		ble.read(deviceId, battery.service, battery.level, app.onReadBatteryLevel, app.onError);
+	},
+	onReadBatteryLevel: function (data) {
+		console.log('onReadBatteryLevel', data);
+		var batteryLevel = new Uint8Array(data);
+		batteryState.innerHTML = batteryLevel[0];
+	},
+	disconnect: function (event) {
+		var deviceId = event.target.dataset.deviceId;
+		ble.disconnect(deviceId, app.showMainPage, app.onError);
+	},
+	showMainPage: function () {
+		mainPage.hidden = false;
+		detailPage.hidden = true;
+	},
+	showDetailPage: function () {
+		mainPage.hidden = true;
+		detailPage.hidden = false;
+	},
+	onError: function (reason) {
+		alert('ERROR: ' + JSON.stringify(reason, null, 4)); // real apps should use notification.alert
+	}
 };
 
 app.initialize();
